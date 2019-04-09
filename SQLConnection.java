ef0af83ea9d3905f71f697533be6059ced94f8fd
Tim Romer <Tim.Romer@gmail.com>
2019-04-09 09:16:33 -0400

    Tried to fix remote connectivity issues

--- SQLConnection.java
+++ SQLConnection.java
@@ -1,27 +1,36 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import javax.sql.*;
 
 
 public class SQLConnection {
 
-	public static void main(String[] args) {
-		 String url = "jdbc:sqlserver://69.133.23.107:3306;databaseName=applist;user=Test201User;password=user201";
-		 try {
-			Connection sqlConnection = DriverManager.getConnection(url);
-			Statement statement = sqlConnection.createStatement();
-			String query = "SELECT name;";
-			ResultSet results = statement.executeQuery(query);
-			
+	public static void main(String[] args) throws ClassNotFoundException {
+		String url = "jdbc:mysql://69.133.23.107:3306/applist"; //databaseName=applist;user=Test201User;password=user201";
+		Connection connect = null;
+		Statement statement = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		 try{
+	         // this will load the MySQL driver, each DB has its own driver
+	       Class.forName("com.mysql.jdbc.Driver");
+	       // setup the connection with the DB.
+	       connect = DriverManager.getConnection(url,"Test201User", "user201");
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		finally {
+			if (connect != null) {
+				try	{
+					connect.close ();
+					System.out.println ("Database connection terminated");
+				}
+				catch (Exception e) { /* ignore close errors */ }
+			}
+		}
 	}
-	
-	
+
+
 
 }
