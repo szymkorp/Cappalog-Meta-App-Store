6545d44bcfc01b1c896f798af25bfcd286cfe21c
Tim Romer <Tim.Romer@gmail.com>
2019-04-09 09:18:11 -0400

    Tried to fix remote connectivity issues

--- SQLConnection.java
+++ SQLConnection.java
@@ -5,35 +5,35 @@ import javax.sql.*;
  * This needs to be run in eclipse with the mysql-connector jar file
  */
 
-public class SQLConnection {
+ public class SQLConnection {
 
-	public static void main(String[] args) throws ClassNotFoundException {
-		String url = "jdbc:mysql://69.133.23.107:3306/applist"; //databaseName=applist;user=Test201User;password=user201";
-		Connection connect = null;
-		Statement statement = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
-		 try{
-	         // this will load the MySQL driver, each DB has its own driver
-	       Class.forName("com.mysql.jdbc.Driver");
-	       // setup the connection with the DB.
-	       connect = DriverManager.getConnection(url,"Test201User", "user201");
+ 	public static void main(String[] args) throws ClassNotFoundException {
+ 		String url = "jdbc:mysql://localhost:3306/cappalogdb"; //databaseName=applist;user=Test201User;password=user201";
+ 		Connection connect = null;
+ 		 try{
+ 	       Class.forName("com.mysql.jdbc.Driver");
+ 	       connect = DriverManager.getConnection(url,"Test201User", "user201");
+ 	       Statement stmt = connect.createStatement();
+ 	       String sql = "Select * From apptable;";
+ 	       ResultSet rs = stmt.executeQuery(sql);
+ 	       if (rs.next()) {
+ 	    	   System.out.println("Congrats it works");
+ 	       }
+ 		} catch (SQLException e) {
+ 			// TODO Auto-generated catch block
+ 			e.printStackTrace();
+ 		}
+ 		finally {
+ 			if (connect != null) {
+ 				try	{
+ 					connect.close ();
+ 					System.out.println ("Database connection terminated");
+ 				}
+ 				catch (Exception e) { /* ignore close errors */ }
+ 			}
+ 		}
+ 	}
 
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		finally {
-			if (connect != null) {
-				try	{
-					connect.close ();
-					System.out.println ("Database connection terminated");
-				}
-				catch (Exception e) { /* ignore close errors */ }
-			}
-		}
-	}
 
 
-
-}
+ }
